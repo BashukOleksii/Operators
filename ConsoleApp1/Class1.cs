39d0985313858b792612291581b707712e8fdc2f
BashukOleksii <oleksiichort@gmail.com>
2025-09-17 23:38:46 +0300

    +,- operators

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Rectangle(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double S()
+        {
+            return X * Y;
+        }
+        public double P()
+        {
+            return 2 * (X + Y);
+        }
+        public void Show()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+
+
+
+        public static Rectangle operator +(Rectangle rectangle, Rectangle rectangle1)
+        {
+            return new Rectangle(rectangle.X + rectangle1.X, rectangle.Y + rectangle1.Y);
+        }
+
+        public static Rectangle operator +(Rectangle rectangle, double num)
+        {
+            return new Rectangle(rectangle.X + num, rectangle.Y + num);
+        }
+
+        public static Rectangle operator +(double num, Rectangle rectangle)
+        {
+            return new Rectangle(rectangle.X + num, rectangle.Y + num);
+        }
+
+        public static Rectangle operator -(Rectangle rectangle, Rectangle rectangle1)
+        {
+            if (rectangle1.X > rectangle.X || rectangle1.Y > rectangle.Y)
+                throw new Exception();
+            return new Rectangle(rectangle.X - rectangle1.X, rectangle.Y - rectangle1.Y);
+        }
+
+        public static Rectangle operator -(Rectangle rectangle, double num)
+        {
+            if(num > rectangle.X || num > rectangle.Y )
+                throw new Exception();
+            return new Rectangle(rectangle.X - num, rectangle.Y - num);
+        }
+
+        public static Rectangle operator -(double num, Rectangle rectangle)
+        {
+            if(num < rectangle.X || num < rectangle.Y)
+                throw new Exception();
+            return new Rectangle(num - rectangle.X,num - rectangle.Y);
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Specialized;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle(10, 10);
+            Rectangle rectangle2 = new Rectangle(30, 20);
+
+            Rectangle rectangle =  rectangle1 - rectangle2;
+            rectangle.Show();
+        }
+    }
+}
